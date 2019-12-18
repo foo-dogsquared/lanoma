32cac81aefc4184c55688aa10e9eca2624ad7a54
foo-dogsquared <foo.dogsquared@gmail.com>
2019-12-18 15:03:55 +0800

    Revise the data structure of the profile
    The profile now has a builder to enable complex and dynamic configuration similar to the shelf. Similarly, the shelf constructor has been updated. The export function now solely holds (or at least should solely hold) the responsibility of creating the profile in the filesystem.
    
    P.S. The 'from' methods might use a little rework implemented as trait methods with the 'From' trait.

--- src/lib.rs
+++ src/lib.rs
@@ -22,19 +22,19 @@ mod helpers;
 pub mod shelf;
 pub mod notes;
 
-use shelf::Shelf;
+use shelf::{ Shelf, ShelfBuilder, ExportOptions };
 use error::Error;
 
 pub type Result<T> = result::Result<T, Error>;
 
 // profile constants 
-const PROFILE_METADATA_FILENAME: &str = "profile.json";
-const PROFILE_COMMON_FILES_DIR_NAME: &str = "common";
-const PROFILE_SHELF_FOLDER: &str = "notes";
-const PROFILE_TEMPLATE_FILES_DIR_NAME: &str = "templates";
+pub const PROFILE_METADATA_FILENAME: &str = "profile.json";
+pub const PROFILE_COMMON_FILES_DIR_NAME: &str = "common";
+pub const PROFILE_SHELF_FOLDER: &str = "notes";
+pub const PROFILE_TEMPLATE_FILES_DIR_NAME: &str = "templates";
 
-const PROFILE_NOTE_TEMPLATE_NAME: &str = "note";
-const PROFILE_MASTER_NOTE_TEMPLATE_NAME: &str = "master";
+pub const PROFILE_NOTE_TEMPLATE_NAME: &str = "note";
+pub const PROFILE_MASTER_NOTE_TEMPLATE_NAME: &str = "master";
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ProfileMetadata {
@@ -48,20 +48,70 @@ pub struct ProfileMetadata {
 impl ProfileMetadata {
     /// Create a new profile metadata instance. 
     pub fn new (
-        name: Option<String>, 
-        extra: Option<HashMap<String, Value>>
     ) -> Self {
         ProfileMetadata {
-            name: match name {
-                Some(v) => v, 
-                None => String::from("New Student"), 
-            }, 
+            name: String::from("New Student"), 
             version: String::from(consts::TEXTURE_NOTES_VERSION), 
-            extra: match extra {
-                Some(v) => v, 
-                None => HashMap::<String, Value>::new(), 
-            }
+            extra: HashMap::<String, Value>::new(), 
         }
+    }
+
+    /// Create a profile metadata instance from a file. 
+    pub fn from_fs<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let metadata_file_string = fs::read_to_string(path).map_err(Error::IoError)?;
+        let metadata_result: Self = serde_json::from_str(&metadata_file_string).map_err(Error::SerdeValueError)?;
+
+        Ok(metadata_result)
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn version(&self) -> &String {
+        &self.version
+    }
+
+    pub fn extra(&self) -> &HashMap<String, Value> {
+        &self.extra
+    }
+
+    /// Sets the name of the profile metadata.
+    pub fn set_name(&mut self, name: String) -> &mut Self {
+        self.name = name;
+        self
+    }
+
+    /// Sets the version of the metadata.
+    pub fn set_version(&mut self, version: String) -> &mut Self {
+        self.version = version;
+        self
+    }
+
+    /// Sets the extra metadata. 
+    pub fn set_extra(&mut self, extra: HashMap<String, Value>) -> &mut Self {
+        self.extra = extra;
+        self
+    }
+
+    /// Merge the extra metadata hashmap into the profile metadata instance. 
+    pub fn merge_extra(&mut self, new_extra: HashMap<String, Value>) -> &mut Self {
+        for (key, value) in new_extra.into_iter() {
+            self.extra.insert(key, value);
+        }
+
+        self
+    }
+
+    /// Export the metadata in the filesystem. 
+    pub fn export<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let path = path.as_ref();
+
+        let mut file = fs::OpenOptions::new().write(true).create(true).open(path).map_err(Error::IoError)?;
+        let metadata_as_string = serde_json::to_string_pretty(&self).map_err(Error::SerdeValueError)?;
+        file.write_all(metadata_as_string.as_bytes()).map_err(Error::IoError)?;
+        
+        Ok(())
     }
 }
 
@@ -157,55 +207,125 @@ pub struct Profile {
     templates: handlebars::Handlebars, 
 }
 
-impl Profile {
-    /// Initializes the data to set up a new profile. 
-   pub fn new<P: AsRef<Path>> (
-       path: P, 
-       name: Option<String>, 
-       use_db: bool
-    ) -> Result<Profile> {
-        let mut path = path.as_ref().to_path_buf();
+/// A builder for constructing the profile. 
+/// Setting the values does not consume the builder for dynamic setting. 
+pub struct ProfileBuilder {
+    path: Option<PathBuf>, 
+    use_db: bool, 
+    name: Option<String>, 
+    extra_metadata: Option<HashMap<String, Value>>,
+}
 
-        if !path.ends_with(consts::TEXTURE_NOTES_DIR_NAME) {
-            path.push(consts::TEXTURE_NOTES_DIR_NAME); 
+impl ProfileBuilder {
+    /// Creates a new profile builder instance. 
+    pub fn new() -> Self {
+        Self {
+            path: None, 
+            use_db: false, 
+            name: None, 
+            extra_metadata: None,
+        }
+    }
+
+    /// Sets the path for the builder. 
+    pub fn path<P>(&mut self, p: P) -> &mut Self 
+        where
+            P: AsRef<Path> + Clone
+    {
+        let p = p.clone().as_ref().to_path_buf();
+
+        self.path = Some(p);
+        self
+    }
+
+    /// Sets if the profile will use the database for the shelf. 
+    pub fn use_db(&mut self, use_db: bool) -> &mut Self {
+        self.use_db = use_db;
+        self
+    }
+
+    /// Sets the name of the profile builder. 
+    pub fn name<S>(&mut self, name: S) -> &mut Self 
+        where 
+            S: AsRef<str> + Clone
+    {
+        let name = name.clone().as_ref().to_string();
+
+        self.name = Some(name);
+        self
+    }
+
+    /// Sets the extra metadata of the profile builder. 
+    pub fn extra_metadata(&mut self, extra: HashMap<String, Value>) -> &mut Self {
+        self.extra_metadata = Some(extra);
+        self
+    }
+
+    /// Consumes the builder to create the profile instance. 
+    pub fn build(self) -> Result<Profile> {
+        let mut profile = Profile::new();
+
+        if self.path.is_some() {
+            let path = self.path.unwrap();
+
+            profile.path = path;
         }
 
-        // create the shelf instance
-        let mut note_folder = path.clone();
-        note_folder.push(PROFILE_SHELF_FOLDER);
-        let notes: Shelf = Shelf::new(note_folder, use_db)?;
+        let mut shelf_builder = ShelfBuilder::new();
+        shelf_builder.path(profile.shelf_path());
 
-        // create the profile instance
-        let metadata: ProfileMetadata = ProfileMetadata::new(name, None);
-        let templates = handlebars::Handlebars::new();
-        let profile = Profile { path, notes, metadata, templates };
+        if self.use_db {
+            shelf_builder.use_db(self.use_db);
+        }
+
+        let mut metadata_instance = ProfileMetadata::new();
+
+        if self.name.is_some() {
+            let name = self.name.unwrap();
+
+            metadata_instance.set_name(name);
+        }
+
+        if self.extra_metadata.is_some() {
+            let extra = self.extra_metadata.unwrap();
+
+            metadata_instance.set_extra(extra);
+        }
+
+        profile.notes = shelf_builder.build()?;
+        profile.metadata = metadata_instance;
 
         Ok(profile)
+    }
+}
+
+impl Profile {
+    /// Creates a profile instance with empty data. 
+   pub fn new() -> Self {
+        Self {
+            path: PathBuf::new(),
+            notes: Shelf::new(), 
+            metadata: ProfileMetadata::new(),
+            templates: handlebars::Handlebars::new(),
+        }
     }
 
     /// Opens an initiated profile. 
     /// 
     /// If the profile does not exist in the given path, it will cause an error. 
-    pub fn from<P: AsRef<Path>> (path: P) -> Result<Profile> {
+    pub fn from<P: AsRef<Path>>(path: P) -> Result<Profile> {
         let mut path: PathBuf = path.as_ref().to_path_buf();
 
-        // if the path is not found or does not have the profile metadata
-        // then it will result in an error
-        if !path.ends_with(consts::TEXTURE_NOTES_DIR_NAME) {
-            path.push(consts::TEXTURE_NOTES_DIR_NAME);
-        }
-
+        // getting the metadata from the metadata file
         let mut metadata_path: PathBuf = path.clone();
         metadata_path.push(PROFILE_METADATA_FILENAME);
+        let metadata = ProfileMetadata::from_fs(metadata_path)?;
 
-        // getting the metadata from the metadata file
-        let metadata_file: File = File::open(metadata_path).map_err(Error::IoError)?;
-        let mut metadata_file_string: String = String::new();
-        let mut metadata_file_buffer = BufReader::new(metadata_file);
-        metadata_file_buffer.read_to_string(&mut metadata_file_string).map_err(Error::IoError)?;
-        
-        let metadata: ProfileMetadata = serde_json::from_str(&metadata_file_string).map_err(Error::SerdeValueError)?;
-        let notes: Shelf = Shelf::new(path.clone(), true)?;
+        // getting the shelf
+        let mut shelf_path = path.clone();
+        shelf_path.push(PROFILE_SHELF_FOLDER);
+        let notes: Shelf = Shelf::from(shelf_path)?;
+
         let templates = handlebars::Handlebars::new();
 
         let mut profile = Profile { path, notes, metadata, templates };
@@ -265,7 +385,7 @@ impl Profile {
                 None => continue, 
             }
 
-            let file_name = match path.file_name() {
+            let file_name = match path.file_stem() {
                 Some(os_name) => match os_name.to_str() {
                     Some(v) => v, 
                     None => continue, 
@@ -303,6 +423,7 @@ impl Profile {
         let mut metadata = serde_json::to_value(self.metadata.clone()).map_err(Error::SerdeValueError)?;
         metadata["title"] = serde_json::json!(note.title());
         metadata["subject"] = serde_json::json!(subject.name());
+        metadata["date"] = serde_json::json!(chrono::Local::now().format("%F").to_string());
 
         let template_name = match template_name {
             Some(v) => v, 
@@ -338,9 +459,12 @@ impl Profile {
         self.metadata_path().exists()
     }
 
-    /// Returns the shelf path of the profile. 
+    /// Returns the preconfigured shelf path of the profile. 
     pub fn shelf_path (&self) -> PathBuf {
-        self.notes.path()
+        let mut path = self.path.clone();
+        path.push(PROFILE_SHELF_FOLDER);
+
+        path
     }
 
     /// Checks if the shelf is in the filesystem. 
@@ -383,17 +507,8 @@ impl Profile {
         Ok(metadata)
     }
 
-    /// Export the metadata in the filesystem. 
-    pub fn export_metadata(&self) -> Result<()> {
-        let mut file = fs::OpenOptions::new().write(true).create(true).open(self.metadata_path()).map_err(Error::IoError)?;
-        let metadata_as_string = serde_json::to_string_pretty(&self.metadata).map_err(Error::SerdeValueError)?;
-        file.write_all(metadata_as_string.as_bytes()).map_err(Error::IoError)?;
-        
-        Ok(())
-    }
-
     /// Export the profile in the filesystem. 
-    pub fn export(&mut self) -> Result<()> {
+    pub fn export(&mut self, export_options: &ExportOptions) -> Result<()> {
         let dir_builder = DirBuilder::new();
 
         if self.is_valid() {
@@ -409,15 +524,15 @@ impl Profile {
         }
 
         if !self.has_metadata() {
-            self.export_metadata()?;
+            self.metadata.export(self.metadata_path())?;
+        }
+        
+        if !self.has_templates() {
+            helpers::filesystem::create_folder(&dir_builder, self.templates_path())?;
         }
 
         if !self.has_shelf() {
-            self.notes.export()?;
-        }
-
-        if !self.has_templates() {
-            helpers::filesystem::create_folder(&dir_builder, self.templates_path())?;
+            self.notes.export(&export_options)?;
         }
 
         Ok(())
@@ -426,8 +541,15 @@ impl Profile {
     /// Create the subjects for the profile. 
     /// 
     /// Underneath, it just calls the same shelf function `shelf.create_subjects` method. 
-    pub fn create_subjects(&self, subjects: &Vec<notes::Subject>, add_to_db: bool, strict: bool) -> Result<Vec<notes::Subject>> {
-        self.notes.create_subjects(&subjects, add_to_db, strict)
+    pub fn create_subjects(
+        &self, 
+        subjects: &Vec<notes::Subject>, 
+        export_options: &ExportOptions,
+    ) -> Result<Vec<notes::Subject>> {   
+        match self.is_valid() {
+            true => self.shelf().create_subjects(&subjects, &export_options),
+            false => Err(Error::InvalidProfileError(self.path())), 
+        }
     }
 
     /// Create the notes for the profile. 
@@ -437,8 +559,7 @@ impl Profile {
         &self, 
         subject: &notes::Subject, 
         notes: &Vec<notes::Note>, 
-        add_to_db: bool, 
-        strict: bool, 
+        export_options: &ExportOptions, 
         template_name: Option<&str>, 
     ) -> Result<Vec<notes::Note>> {
         let mut created_notes: Vec<notes::Note> = vec![];
@@ -446,7 +567,7 @@ impl Profile {
         for note in notes.iter() {
             let template_string = self.return_string_from_note_template(&subject, &note, template_name)?;
 
-            if self.notes.create_note(&subject, &note, &template_string, add_to_db, strict).is_ok() {
+            if self.notes.create_note(&subject, &note, &template_string, &export_options).is_ok() {
                 created_notes.push(note.clone())
             }
         }
@@ -567,6 +688,28 @@ impl Profile {
             None => PROFILE_DEFAULT_COMMAND, 
         }
     }
+
+    /// List the entries of the subjects and notes from the shelf database. 
+    pub fn list_entries(&self, order: Option<&str>, date: bool) -> Result<Vec<(notes::Subject, Vec<notes::Note>)>> {
+        let mut subjects = self.shelf().get_all_subjects_from_db(order.clone())?;
+        let mut subject_note_tuple_result: Vec<(notes::Subject, Vec<notes::Note>)> = vec![]; 
+
+        if date {
+            subjects = notes::Subject::sort_by_date(self.shelf(), &subjects);
+        }
+
+        for subject in subjects {
+            let mut notes_in_shelf = self.shelf().get_all_notes_by_subject_from_db(&subject, None)?;
+
+            if date {
+                notes_in_shelf = notes::Note::sort_by_date(self.shelf(), &subject, &notes_in_shelf);
+            }
+
+            subject_note_tuple_result.push((subject, notes_in_shelf));
+        }
+
+        Ok(subject_note_tuple_result)
+    }
 }
 
 
@@ -576,12 +719,17 @@ mod tests {
 
     #[test]
     fn profile_init_test() -> Result<()> {
-        let test_folder_name = format!("./tests/{}", consts::TEXTURE_NOTES_DIR_NAME);
+        let test_folder_name = format!("tests/{}", consts::TEXTURE_NOTES_DIR_NAME);
         let test_path = PathBuf::from(&test_folder_name);
         fs::remove_dir_all(&test_path);
-        let mut test_profile: Profile = Profile::new(&test_path, None, true)?;
+        let mut test_profile_builder = ProfileBuilder::new();
+        test_profile_builder.path(test_path.clone()).use_db(true);
         
-        assert_eq!(test_profile.export().is_ok(), true);
+        let mut test_profile = test_profile_builder.build()?;
+        let mut export_options = ExportOptions::new();
+        export_options.include_in_db(true).with_metadata(true);
+        
+        assert_eq!(test_profile.export(&export_options).is_ok(), true);
         
         let test_subjects = notes::Subject::from_vec_loose(&vec!["Calculus", "Algebra", "Physics"], &test_profile.notes)?;
         let test_notes = notes::Note::from_vec_loose(
@@ -599,26 +747,31 @@ mod tests {
         \end{document}
         ";
 
-        test_profile.notes.create_subjects(&test_subjects, true, false)?;
-        test_profile.notes.create_notes(&test_subjects[0], &test_notes, test_input, true, false)?;
+        test_profile.notes.create_subjects(&test_subjects, &export_options)?;
+        test_profile.notes.create_notes(&test_subjects[0], &test_notes, test_input, &export_options)?;
         test_profile.compile_notes_in_parallel(&test_subjects[0], &test_notes, 2)?;
-        assert_eq!(test_profile.symlink_to_common_path_from_subject(&test_subjects[0]), PathBuf::from("./tests/texture-notes-profile/notes/calculus/common"));
+        assert_eq!(test_profile.symlink_to_common_path_from_subject(&test_subjects[0]), PathBuf::from("tests/texture-notes-profile/notes/calculus/common"));
 
         assert!(test_profile.create_symlink_from_common_files_folder_with_subject(&test_subjects[0]).is_ok());
 
-        let test_folder_name = PathBuf::from("./tests/");        
+        assert!(test_profile.list_entries(None, true).is_ok());
+
+        let test_folder_name = PathBuf::from(test_path);        
         assert_eq!(Profile::from(test_folder_name).is_ok(), true);
         
         Ok(())
     }
 
     #[test]
-    fn invalid_profile_init_test() -> Result<()> {
+    #[should_panic]
+    fn invalid_profile_init_test() {
         let test_path = PathBuf::from("./this/path/does/not/exists/");
-        let mut test_profile_result = Profile::new(&test_path, None, true)?;
+        let mut test_profile_builder = ProfileBuilder::new();
+        test_profile_builder.path(&test_path).use_db(true);
 
-        assert!(test_profile_result.export().is_err());
-        Ok(())
+        let mut test_profile = test_profile_builder.build().unwrap();
+        
+        assert!(test_profile.export(&ExportOptions::new()).is_ok());
     }
 
     #[test]
